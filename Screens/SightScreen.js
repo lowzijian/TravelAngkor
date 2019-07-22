06821d6536b4971efff3dbe2329513d82ea95029
Jake <lowbak1998@hotmail.com>
2019-07-22 22:29:40 +0800

    add in sqllite

--- Screens/SightScreen.js
+++ Screens/SightScreen.js
@@ -3,19 +3,28 @@ import { Text, View, Image} from 'react-native'
 import HeaderImageScrollView from 'react-native-image-header-scroll-view';
 import * as theme from '../utils/theme';
 import { Button } from 'react-native-elements';
+import Database from '../utils/database';
+
+const db = new Database();
 
 
-
-export default class DiscoverScreen extends Component {
+export default class SightScreen extends Component {
 
   constructor(props) {
     super(props);
     this.toggleVisited = this.toggleVisited.bind(this);
-    this.state = {
-      isVisited:false
-    };
+
+  this.state ={
+      sight: this.props.navigation.getParam('sight'),
+      isVisited:0}
   }
 
+  componentDidMount() {
+    this.getIsVisited();
+  }
+  componentWillUnmount(){
+    this.updateVisited();
+  }
 
   toggleVisited() {
     this.setState({
@@ -23,11 +32,26 @@ export default class DiscoverScreen extends Component {
     })
   }
 
+  getIsVisited() {
+    let visitToggle = 0
+    db.isVisitedByTitle(this.state.sight.title).then((data) => {
+      visitToggle = data;
+      console.log(visitToggle)
+      this.setState({
+        isVisited:visitToggle
+      })
+    }).catch((err) => {
+    console.log(err);
+    this.setState = {
+    }
+  })
+}
+
+updateVisited(){
+  db.updateSightVisited(this.state.isVisited)
+}
+
   render() {
-    const { navigation } = this.props;
-    const sight = navigation.getParam('sight');
-
-
 
     return (
     <HeaderImageScrollView
@@ -35,19 +59,19 @@ export default class DiscoverScreen extends Component {
       maxHeight={theme.MAX_HEIGHT}
       minHeight={0}      
       scrollViewBackgroundColor= "transparent"
-      renderHeader={() => <Image source={{uri: sight.preview}} style={theme.styling.backgroundImage} />}>
+      renderHeader={() => <Image source={{uri: this.state.sight.preview}} style={theme.styling.backgroundImage} />}>
       
 
         <View style={[theme.styling.contentHeight,{height:500}]}>
             <View style={[theme.styling.contentContainer]}>
-                    <Text style={theme.styling.title2}>{sight.title}</Text>
-                    <Text style={theme.styling.caption}>{sight.subtitle}</Text>
+                    <Text style={theme.styling.title2}>{this.state.sight.title}</Text>
+                    <Text style={theme.styling.caption}>{this.state.sight.subtitle}</Text>
             </View>
               <View style={theme.styling.contentContainer}>
-                  <Text style={theme.styling.description}>{sight.description}</Text>
+                  <Text style={theme.styling.description}>{this.state.sight.description}</Text>
               </View>
               <View style={[theme.styling.contentContainer,{marginTop:25}]}>
-                  <Button  type="outline" title= {this.state.isVisited ? 'Visited' : 'Visit'} buttonStyle = {[{borderColor:"grey", backgroundColor:this.state.isVisited ? 'white' : 'grey',borderRadius:theme.sizes.radius} ,theme.styling.shadow]} titleStyle ={{color:this.state.isVisited ? 'grey' : 'white'}}
+                  <Button  type="outline" title= {this.state.isVisited ? 'Visited' : 'Visit'} buttonStyle = {[{borderColor:"grey", backgroundColor:this.state.isVisited ? 'lightgrey' : 'white',borderRadius:theme.sizes.radius} ,theme.styling.shadow]} titleStyle ={{color:this.state.isVisited ? 'white' : 'grey'}}
                      onPress={() =>{this.toggleVisited()}}/>
               </View>
         </View>
---
+++ utils/database.js
@@ -0,0 +1,67 @@
+import SQLite from "react-native-sqlite-storage";
+
+SQLite.DEBUG(true);
+SQLite.enablePromise(true);
+
+export default class Database {
+
+  closeDatabase(db) {
+    if (db) {
+      console.log("Closing DB");
+      db.close()
+      .then(status => {
+        console.log("Database CLOSED");
+      })
+      .catch(error => {
+        this.errorCB(error);
+      });
+    } else {
+      console.log("Database was not OPENED");
+    }
+  };
+
+isVisitedByTitle(title) {
+    console.log(title);
+    return new Promise((resolve) => {
+        SQLite.openDatabase({name: 'sightdb', createFromLocation : '~db.sqlite'}).then((DB) => {
+        db = DB;
+        db.transaction((tx) => {
+          tx.executeSql('SELECT isVisited FROM sights WHERE title = ?', [title]).then(([tx,results]) => {
+            console.log("Query completed");
+            console.log(results);
+            if(results.rows.length > 0) {
+              let row = results.rows.item(0);
+              resolve(row);
+            
+            }
+          });
+        }).then((result) => {
+          this.closeDatabase(db);
+        }).catch((err) => {
+          console.log(err);
+        });
+      }).catch((err) => {
+        console.log(err);
+      });
+    });  
+  }
+  updateSightVisited(isVisited, title) {
+    return new Promise((resolve) => {
+      SQLite.openDatabase({name: 'sightdb', createFromLocation : '~db.sqlite'}).then((DB) => {
+        db = DB;
+        db.transaction((tx) => {
+          tx.executeSql('UPDATE sights SET isVisited = ? WHERE title = ?', [isVisited, title]).then(([tx, results]) => {
+            console.log("Update completed");
+            resolve(results);
+          });
+        }).then((result) => {
+          this.closeDatabase(db);
+        }).catch((err) => {
+          console.log(err);
+        });
+      }).catch((err) => {
+        console.log(err);
+      });
+    });  
+  }
+}
